44a99689bfafc3975eb6cbb2db21a610711eba3d
patrick-castro <castropatrickantonio@gmail.com>
2023-09-15 14:05:38 +0800

    Explore basics in the Dashboard page

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,38 @@
+import { Box, Container, Heading, Text } from '@chakra-ui/react'
+
 export default function Dashboard() {
+  const boxStyles = {
+    p: '10px',
+    bg: 'purple.400',
+    color: 'white',
+    m: '10px',
+    textAlign: 'center',
+
+    // Use sx if the style prop is not available in the Chakra-UI component
+    filter: 'blur(2px)',
+    ':hover': {
+      color: 'black',
+      bg: 'blue.200',
+    },
+  }
+
   return (
-    <div>Dashboard</div>
+    <Container as='section' maxWidth='4xl' py='20px'>
+      <Heading my='30px' p='10px'>
+        Chakra UI Components
+      </Heading>
+      <Text marginLeft='30px'>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+      </Text>
+      <Text ml='30px' color='blue.300' fontWeight='bold'>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+      </Text>
+
+      <Box my='30px' p='20px' bg='orange'>
+        <Text color='white'>This is a box</Text>
+      </Box>
+
+      <Box sx={boxStyles}>Hello, Ninjas!</Box>
+    </Container>
   )
 }
